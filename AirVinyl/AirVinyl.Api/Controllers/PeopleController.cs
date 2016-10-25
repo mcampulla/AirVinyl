05eff8d123a25ad9a2227aa5a27fdad7ba2a1cbe
Marco Campulla <mcampulla@gmail.com>
2016-10-25 18:11:25 +0200

    Get single entity and property

--- AirVinyl/AirVinyl.Api/Controllers/PeopleController.cs
+++ AirVinyl/AirVinyl.Api/Controllers/PeopleController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.OData;
+using System.Web.OData.Routing;
+using AirVinyl.Api.Helpers;
 
 namespace AirVinyl.Api.Controllers
 {
@@ -26,6 +28,92 @@ namespace AirVinyl.Api.Controllers
                 return Ok(person);
             else
                 return NotFound();
+        }
+
+        [HttpGet]
+        [ODataRoute("People({key})/Email")]
+        [ODataRoute("People({key})/FirstName")]
+        [ODataRoute("People({key})/LastName")]
+        [ODataRoute("People({key})/DateOfBirth")]
+        [ODataRoute("People({key})/Gender")]
+        public IHttpActionResult GetPersonProperty([FromODataUri]int key)
+        {
+            var person = _ctx.People.FirstOrDefault(p => p.PersonId == key);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var propertyToGet = Url.Request.RequestUri.Segments.Last();
+            if (!person.HasProperty(propertyToGet))
+            {
+                return NotFound();
+            }
+
+            var propertyValue = person.GetValue(propertyToGet);
+            if (propertyValue == null)
+            {
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+
+            return this.CreateOKHttpActionResult(propertyValue);
+        }
+
+        [HttpGet]
+        [ODataRoute("People({key})/Friends")]
+        [ODataRoute("People({key})/VinylRecords")]
+        public IHttpActionResult GetPersonCollectionProperty([FromODataUri]int key)
+        {
+            var collectionPropertyToGet = Url.Request.RequestUri.Segments.Last();
+            var person = _ctx.People.Include(collectionPropertyToGet)
+                .FirstOrDefault(p => p.PersonId == key);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            if (!person.HasProperty(collectionPropertyToGet))
+            {
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+
+            var collectionPropertyValue = person.GetValue(collectionPropertyToGet);
+            return this.CreateOKHttpActionResult(collectionPropertyValue);
+        }
+
+
+        [HttpGet]
+        [ODataRoute("People({key})/Email/$value")]
+        [ODataRoute("People({key})/FirstName/$value")]
+        [ODataRoute("People({key})/LastName/$value")]
+        [ODataRoute("People({key})/DateOfBirth/$value")]
+        [ODataRoute("People({key})/Gender/$value")]
+        public IHttpActionResult GetPersonRawProperty([FromODataUri]int key)
+        {
+            var person = _ctx.People.FirstOrDefault(p => p.PersonId == key);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            var propertyToGet = Url.Request.RequestUri.Segments[Url.Request.RequestUri.Segments.Length -2].TrimEnd('/');
+            if (!person.HasProperty(propertyToGet))
+            {
+                return NotFound();
+            }
+
+            var propertyValue = person.GetValue(propertyToGet);
+            if (propertyValue == null)
+            {
+                return StatusCode(System.Net.HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return this.CreateOKHttpActionResult(propertyValue.ToString());
+            }
         }
 
         protected override void Dispose(bool disposing)
